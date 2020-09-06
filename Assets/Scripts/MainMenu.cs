c216e43398b104094c23caa6c74b0f1c202d786e
RobMico <55360403+RobMico@users.noreply.github.com>
2020-09-06 19:10:22 +0300

    Main menu
    Hmmmmmm

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject Settings;   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnStartGameClick() {
+        Debug.Log("4");
+        SceneManager.LoadScene("RoguelikeScene");
+    }
+    public void OnSettingsClick()
+    {
+        Debug.Log("3");
+        Settings.SetActive(true);
+    }
+    public void OnExitClick()
+    {
+        Debug.Log("2");
+        Application.Quit();
+    }
+    public void OnSettingsReturnClick()
+    {
+        Debug.Log("1");
+        Settings.SetActive(false);
+    }
+}
